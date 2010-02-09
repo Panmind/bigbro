d7049aade3a6fad34022f01d63bc6bae2cc29109
Paolo Zaccagnini <paozac@gmail.com>
2010-02-09 12:31:53 +0100

    Cucumber: first working integration test

--- lib/pm/piwik.rb
+++ lib/pm/piwik.rb
@@ -22,7 +22,7 @@ module PM
       #     :i_really_want_to_disable_analytics: true
       #
       raise ArgumentError, 'Analytics configuration missing' if Config.blank?
-    elsif Rails.env.test?
+    elsif Rails.env.test? || Rails.env == 'cucumber'
       Config = {:url => 'http://test.host/piwik', :id => 420}
     end
 
