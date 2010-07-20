253c13f6be9f6f784a8ff5023b52ceb05db84354
Marcello Barnaba <vjt@openssl.it>
2010-07-20 14:52:46 +0200

    Moved the plugin file under the panmind/ namespace

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require 'bigbro'
+require 'panmind/bigbro'
 
 ActionView::Base.instance_eval { include Panmind::BigBro::Helpers }
 ActiveSupport::TestCase.instance_eval { include Panmind::BigBro::TestHelpers } if Rails.env.test?
